694294b7e942cf2afc159c89ee0ac111c82910ba
Stiven Julio Doval <stjuliod@unal.edu.co>
2023-02-18 20:41:01 -0500

    Project
    Se crearon clases, métodos y atributos

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int CantidadCreados;
+	
+	int cantidadAsientos() {
+		
+		int numAsientos = 0;
+		
+		for (int i =0 ; i < asientos.length; i++) {
+			
+			if (asientos[i]
+			
+			
+		}
+				
+	}
+	
+
+}                     
